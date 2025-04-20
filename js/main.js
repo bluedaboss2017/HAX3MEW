15c443876514e1b2804ebe62ae7571a81b0e7b64
bluedaboss2017 <57729579+bluedaboss2017@users.noreply.github.com>
2025-04-20 17:09:03 -0500

    Update main.js
    Utilized all stats returned by the API

--- js/main.js
+++ js/main.js
@@ -8,14 +8,12 @@ const youtubeUser = 'UCXAcZDxhaZDJYdy27O0BuBA';
 const textToUpdate = document.querySelector('.firstP');
 
 let getSubscribers = () => {
-    fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUser}&key=${key}`)
-    .then(response => {
+    fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUser}&key=${key}`).then(response => {
             return response.json()
-    })
-    .then(data => {
+    }).then(data => {
             textToUpdate.innerText = `I'm HAX3MEW, and I run a gaming YouTube channel that is currently at ${data["items"][0].statistics.subscriberCount} subscribers. I occasionally upload videos, but I mainly stream with friends, currently on Fridays and Weekends.`
-    });
+            document.querySelector('.bottom').innerText = `Current Stats: ${data["items"][0].statistics.viewCount} views, ${data["items"][0].statistics.subscriberCount} Subscribers, ${data["items"][0].statistics.videoCount} videos`
+        });
 };
-
 
 window.onload = getSubscribers;
