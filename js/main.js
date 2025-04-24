2c356a99775ad0369f1409c89f1fe572fed40690
bluedaboss2017 <57729579+bluedaboss2017@users.noreply.github.com>
2025-04-24 18:12:49 -0500

    Updated main.js
    Removed temporary event handler as new pages will be made soon

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
-document.querySelector('button').addEventListener('click', function() {
-    alert("You're already on the home page goofy, I haven't added more pages yet");
-});
-
-// I am aware that leaving API keys exposed is very bad practice, however the key has been configured on Google's end to minimize risks
+// I am aware that leaving API keys exposed is very bad practice, however the key was free and has been configured on Google's end to minimize risks
 const key = 'AIzaSyCC-fo-cGI56xPLSUbWBb9MXmRc5itP9Nc';
 const youtubeUser = 'UCXAcZDxhaZDJYdy27O0BuBA';
 const textToUpdate = document.querySelector('.firstP');
 
-let getSubscribers = () => {
+getSubscribers = () => {
     fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUser}&key=${key}`).then(response => {
             return response.json()
     }).then(data => {
